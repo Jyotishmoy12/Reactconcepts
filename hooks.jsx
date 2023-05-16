1d17235078f79e6342bcd9b4d66d430838dc1930
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-05-16 21:25:05 +0530

    Create hooks.jsx

---
+++ hooks.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { useState } from 'react';
+const App=() =>{
+  // first name of the state
+  // seter function
+  // inside the usestate provide initial value
+ const [counter, setCounter]=useState(0);
+  return (
+    <div className="App">
+    <button onClick={()=>setCounter((prevCount)=>prevCount-1)}>-</button>
+     <h1>{counter}</h1>
+     <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
+     </div>
+  );
+}
+
+export default App;
