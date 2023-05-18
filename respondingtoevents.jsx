c62fab7d97021aee1f344c3e5ab1cd177e71b084
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-05-18 19:49:19 +0530

    Create respondingtoevents.jsx

---
+++ respondingtoevents.jsx
@@ -0,0 +1,13 @@
+function MyButton(){
+  function handleClick(){
+    alert('You clicked me')
+  }
+  return (
+   <button
+   onClick={handleClick}
+   >
+    click me
+   </button>
+  )
+}
+export default MyButton
